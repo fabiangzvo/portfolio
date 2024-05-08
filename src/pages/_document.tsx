2b710d4011c3061e1569d128ce6ce25619735f01
fabiangzvo <faguzman.97@gmail.com>
2024-05-08 14:17:44 -0500

    add vercel analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Document() {
   return (
     <Html suppressHydrationWarning>
       <Head />
       <body className="absolute lg:w-full flex flex-col justify-center items-center">
+        <Analytics />
         <Main />
         <NextScript />
       </body>
